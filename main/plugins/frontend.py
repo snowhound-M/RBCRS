479d0b2ac4751a34b6e90c7a810277c4cd8cac1e
snowhound-M <atulpundir147@gmail.com>
2024-07-05 12:55:47 +0530

    Update frontend.py

--- main/plugins/frontend.py
+++ main/plugins/frontend.py
@@ -14,7 +14,7 @@ from pyrogram.errors import FloodWait
 from ethon.telefunc import force_sub
 
 ft = f"To use this bot you've to join @{fs}."
-global GL, UI 
+global UI 
 
 message = "Send me the message link you want to start saving from, as a reply to this message."
 
